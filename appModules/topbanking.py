17f9ee93d2273d7001a8f67637de8dcaeaabb70a
Ralf Kefferpuetz <novalis7747@live.com>
2018-06-14 10:10:38 +0200

    Initial commit

---
+++ appModules/topbanking.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+# topbanking: a NVDA appModule for topbanking booking listviews
+#Copyright (C) 2018 Ralf Kefferpuetz
+# Released under GPL 2
+
+import appModuleHandler
+from NVDAObjects.IAccessible import IAccessible
+import speech
+import controlTypes
+import ui
+import scriptHandler
+
+"""
+topbanking enhanced Version 0.1 
+Author: Ralf Kefferpuetz, June 2018
+Features:
+- reducing speaking of the cell grid to its value
+- adds a hotkey Alt-1 to read the cell header and line number
+"""
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName in ("WindowsForms10.Window.8.app.0.34dca42_r6_ad1", "WindowsForms10.Window.8.app.0.7f9478_r6_ad1"):
+			clsList.insert(0, TBGrid)
+
+class TBGrid(IAccessible):
+
+	def event_gainFocus(self):
+		if self.windowClassName == "WindowsForms10.Window.8.app.0.34dca42_r6_ad1":
+			if self.IAccessibleRole == 29:
+				ui.message(self.value)
+				return
+		super(TBGrid,self).event_gainFocus()
+
+	def script_readActive(self, gesture):
+		try:
+			ui.message(self.name)
+		except:
+			pass
+	# Translators: Documentation for Alt-1 script.
+	script_readActive.__doc__=_("topbanking enhanced: speaks header and line number of the current cell.")
+
+	__gestures = {
+		"kb:alt+1": "readActive"
+	}
